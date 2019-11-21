e53dda70413f2babf09c8a252e578383cda594a2
Till Hainbach <till.hainbach@uni-potsdam.de>
2019-11-21 21:47:55 +0100

    Initial Commit

---
+++ FastEMD/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FastEMD
+//
+//  Created by Till Hainbach on 21.11.19.
+//  Copyright © 2019 Till Hainbach. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
