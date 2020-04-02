210067f3cac6b519992a75a694b83211489b96af
Till Hainbach <till.hainbach@uni-postdam.de>
2020-04-02 14:01:07 +0200

    changed type to unsigned int

--- utils/utils.h
+++ utils/utils.h
@@ -31,23 +31,22 @@ auto getMaxCost(const _T& costMatrix, const NODE_T N)
 }
 
 inline
-int calculateCostMatVector(int im1_R, int im1_C, int im2_R, int im2_C,
+uint calculateCostMatVector(int im1_R, int im1_C, int im2_R, int im2_C,
                            std::vector< std::vector<int> >& costmat,
                            const int THRESHOLD, const int COST_MULT_FACTOR)
 {
-    int maxCost = -1;
-    int j = -1;
+    uint maxCost = 0;
+    uint j = 0;
     for (unsigned int c1 = 0; c1 < im1_C; ++c1)
     {
-        for (unsigned int r1=0; r1<im1_R; ++r1)
+        for (unsigned int r1=0; r1<im1_R; ++r1, ++j)
         {
-            ++j;
-            int i = -1;
+            uint i = 0;
             for (unsigned int c2 = 0; c2 < im1_C; ++c2)
             {
-                for (unsigned int r2=0; r2<im1_R; ++r2)
+                for (unsigned int r2=0; r2<im1_R; ++r2, ++i)
                 {
-                    ++i;
+                    
                     double L1 = sqrt((r1-r2)*(r1-r2)+(c1-c2)*(c1-c2));
                     costmat[i][j] = std::min(THRESHOLD, static_cast<int>(COST_MULT_FACTOR * L1));
                     if (costmat[i][j] > maxCost) maxCost = costmat[i][j];
